90a252d9af85e401eea31d849db9a6b282c131fd
Bhushan Chaudhari <bhushan.chaudhari@morningstar.com>
2022-07-01 11:17:08 +0530

    Ghe bho

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <nav class="navbar navbar-dark bg-primary justify-content-between flex no-wrap flex-row">
+        <div class="container mt-3">
+          <a class="navbar-brand float-left">Employee Payroll APP</a>
+          <ul class="nav navbar-nav flex-row">
+
+            <li class="active nav-item">
+              <router-link class="active nav-link pr-3" to="/">Home</router-link>
+            </li>
+           
+            <li class="nav-item">
+              <router-link class="nav-link" to="/addCustomer">Add Employee</router-link>
+            </li>
+
+             <li class="nav-item">
+              <router-link class="nav-link" to="/customers">Employees</router-link>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+
+
+      <!--Router View-->
+      <div class="container mt-5">
+        <router-view></router-view>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name:'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.navbar-nav > li{
+  padding-left: 10px;
+}
+.navbar-dark .nav-item > .nav-link.active{
+  color:white;
 }
 </style>
